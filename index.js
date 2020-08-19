2ab9da0a8c739f9a35c6a783eccf85985e3e8076
Erica Schimmel <ericaschimmel01@gmail.com>
2020-08-19 04:19:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ function findMinAndRemove(array){
   let currentMin = array[0];
   let minIdx = 0;
   
-  array.forEach((number) => {
-    
-  })
+  array.forEach((number, idx) => {
+    if (number < currentMin) {
+      currentMin = number;
+      minIdx = idx;
+    }
+  });
 }
 
 function selectionSort(array){
