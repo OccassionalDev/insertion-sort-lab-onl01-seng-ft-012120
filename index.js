2f840bb189e5d35f9fad8975bffe0035294c06dd
Erica Schimmel <ericaschimmel01@gmail.com>
2020-08-19 04:23:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 function findMinAndRemove(array){
-
+  let currentMin = array[0];
+  let minIdx = 0;
+  
+  array.forEach((number, idx) => {
+    if (number < currentMin) {
+      currentMin = number;
+      minIdx = idx;
+    }
+  });
+  
+  array.splice(minIdx, 1);
+  return currentMin;
 }
 
 function selectionSort(array){
-
+  let currentMin;
+  let sortedArray = [];
+  
+  while (array.length != 0) {
+    currentMin = findMinAndRemove(array);
+    sortedArray.push(currentMin);
+  }
+  
+  return sortedArray;
 }
